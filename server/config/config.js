bc3cdf05b8d0203e7a162f020673675cb864334e
root <root@localhost.localdomain>
2019-04-11 00:56:40 +0200

    Add configuration file for the project needs

---
+++ server/config/config.js
@@ -0,0 +1,10 @@
+const config = {
+    env: process.env.NODE_ENV || 'development',
+    port: process.env.PORT || 3000,
+    jwtSecret: process.env.JWT_SECRET || "%$DGHFDIilnh#@#$D",
+    mongoUri: process.env.MONGODB_URI ||
+        process.env.MONGO_HOST ||
+        'mongodb://' + (process.env.IP || 'localhost') + ':' +
+        (process.env.PORT || '27017') +
+        '/ciatdb'
+}
