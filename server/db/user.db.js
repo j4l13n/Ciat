cdc53d603408650beddc7098285c179b819aacf0
root <root@localhost.localdomain>
2019-04-11 01:23:14 +0200

    Add User mockdb

---
+++ server/db/user.db.js
@@ -0,0 +1,10 @@
+const user = {
+    id: 1,
+    fullname: "Karangwa Hirwa Julien",
+    email: "juliushirwa@gmail.com",
+    password: "Regedit56",
+    createOn: Date.now(),
+    isAdmin: false
+};
+
+export default user;
