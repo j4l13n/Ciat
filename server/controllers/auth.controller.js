61ec8c4e934b9dc42823d840603e6e9230f96836
root <root@localhost.localdomain>
2019-04-11 14:13:05 +0200

    Authorizing signed in users

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import User from '../models/user.model'
 import jwt from 'jsonwebtoken'
 import expressJwt from 'express-jwt'
 import config from './../config/config'
+import { runInNewContext } from 'vm';
 
 const signin = (req, res) => {
     User.findOne({
@@ -49,8 +50,14 @@ const requireSignin = expressJwt({
     userProperty: 'auth'
 });
 
-const hasAuthorization = (req, res) => {
-    
+const hasAuthorization = (req, res, next) => {
+    const authorized = req.profile && req.auth && req.profile._id === req.auth._id
+    if (!(authorized)) {
+        return res.status(403).json({
+            error: "User is not authorized"
+        })
+    }
+    next()
 }
 
 export default {
