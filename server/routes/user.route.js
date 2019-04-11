2ffb9d7af271fcf5a77be18fbc55c883d981b3a9
root <root@localhost.localdomain>
2019-04-11 14:18:04 +0200

    Protect user route

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,5 +1,6 @@
-import express from 'express';
-import userCtrl from '../controllers/user.controller';
+import express from 'express'
+import userCtrl from '../controllers/user.controller'
+import authctrl from '../controllers/auth.controller'
 
 const router = express.Router()
 
@@ -8,9 +9,9 @@ router.route('/api/users')
     .post(userCtrl.create)
 
 router.route('/api/users/:userId')
-    .get(userCtrl.read)
-    .put(userCtrl.update)
-    .delete(userCtrl.remove);
+    .get(authctrl.requireSignin, userCtrl.read)
+    .put(authctrl.requireSignin ,userCtrl.update)
+    .delete(authctrl.requireSignin ,userCtrl.remove)
 
 router.param('userId', userCtrl.userById)
 
