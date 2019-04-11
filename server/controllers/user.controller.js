d890f9a74dbe7aaa97afca92c8273f652317cdfa
root <root@localhost.localdomain>
2019-04-11 12:52:47 +0200

    [ft] add user crud api operations

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -28,17 +28,49 @@ const list = (req, res) => {
 }
 
 const userById = (req, res, next, id) => {
-
+    User.findById(id).exec((err, user) => {
+        if (err || !user) {
+            return res.status(400).json({
+                error: "User not found"
+            })
+        }
+        req.profile = user
+        next()
+    })
 }
 
 const read = (req, res) => {
-
+    req.profile.hashed_password = undefined
+    req.profile.salt = undefined
+    return res.json(req.profile)
 }
 const update = (req, res, next) => {
-
+    let user = req.profile
+    user = _.extend(user, req.body)
+    user.updated = Date.now()
+    user.save((err) => {
+        if (err) {
+            return res.status(400).json({
+                error: errorHandler.getErrorMessage(err)
+            })
+        }
+        user.hashed_password = undefined
+        user.salt = undefined
+        res.json(user)
+    })
 }
 const remove = (req, res, next) => {
-
+    let user = req.profile
+    user.remove((err, deleteUser) => {
+        if (err) {
+            return res.status(400).json({
+                error: errorHandler.getErrorMessage(err)
+            })
+        }
+        deleteUser.hashed_password = undefined
+        deleteUser.salt = undefined
+        res.json(deleteUser)
+    })
 }
 
 export default {
